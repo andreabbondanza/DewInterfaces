b01d28483e1387f64a0d0cc7b254dded97c1f326
Andrea Vincenzo Abbondanza <andreabbondanza.developer@outlook.com>
2017-03-19 21:21:34 +0100

    Added MySQL Interfaces

---
+++ DewInterfaces/DewLogger.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DewInterfaces
+{
+    namespace DewLogger
+    {
+        /// <summary>
+        /// Debug interface
+        /// </summary>
+        public interface IDewLogger
+        {
+            /// <summary>
+            /// Write in output the string
+            /// </summary>
+            /// <param name="text"></param>
+            void Write(string text);
+            /// <summary>
+            /// Write in output the string with args
+            /// </summary>
+            /// <param name="text"></param>
+            /// <param name="args"></param>
+            void Write(string text, object[] args);
+            /// <summary>
+            /// Write in output the string with a new line
+            /// </summary>
+            /// <param name="text"></param>
+            void WriteLine(string text);
+            /// <summary>
+            /// Write in output the string with args and a new line
+            /// </summary>
+            /// <param name="text"></param>
+            /// /// <param name="args"></param>
+            void WriteLine(string text, object[] args);
+        }
+    }
+
+}
+
---
+++ DewInterfaces/DewMySQLClient.cs
@@ -0,0 +1,80 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Common;
+
+namespace DewInterfaces.DewDatabase.MySQL
+{
+    /// <summary>
+    /// MySQL Response object for INSERT\DELETE\UPDATE
+    /// </summary>
+    public interface IMySQLResponse
+    {
+        long GetLastInsertedRow();
+        long GetRowsAffected();
+    }
+    /// <summary>
+    /// MySQLClient dew interface
+    /// </summary>
+    public interface IMySQLClient
+    {
+        /// <summary>
+        /// Perform a query and return result in a list of array
+        /// </summary>
+        /// <typeparam name="T">Result type object</typeparam>
+        /// <param name="query">Query</param>
+        /// <param name="values">List of binded values</param>
+        /// <returns>List of array objects (rows)</returns>
+        Task<List<object[]>> QueryArrayAsync(string query, List<DbParameter> values);
+        /// <summary>
+        /// Perform a query (good for SELECT) : 
+        /// </summary>
+        /// <typeparam name="T">Result type object</typeparam>
+        /// <param name="query">Query</param>
+        /// <param name="values">List of binded values</param>
+        /// <returns>List of T objects (rows)</returns>
+        Task<List<T>> QueryAsync<T>(string query, List<DbParameter> values) where T : class, new();
+        /// <summary>
+        /// Select directly in LINQ. NOTE: T name must be the Table Name
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="predicate"></param>
+        /// <param name="tablePrefix"></param>
+        /// <returns></returns>
+        Task<List<T>> Select<T>(Func<T, bool> predicate, string tablePrefix) where T : class, new();
+        /// <summary>
+        /// Select directly in LINQ. NOTE: T name must be the Table Name
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="tablePrefix"></param>
+        /// <returns></returns>
+        Task<List<T>> Select<T>(string tablePrefix) where T : class, new();
+        /// <summary>
+        /// Perform a query (good for insert, update, delete)
+        /// </summary>
+        /// <param name="query"></param>
+        /// <param name="values"></param>
+        /// <returns></returns>
+        Task<IMySQLResponse> QueryAsync(string query, List<DbParameter> values);
+        /// <summary>
+        /// Commit a transaction
+        /// </summary>
+        Task CommitAsync();
+        /// <summary>
+        /// Rollback a transaction
+        /// </summary>
+        Task RoolbackAsync();
+        /// <summary>
+        /// Begin a transiction
+        /// </summary>
+        /// <returns></returns>
+        Task<bool> BeginTransactionAsync(IsolationLevel isolationLevel);
+        /// <summary>
+        /// Close database connection
+        /// </summary>
+        void CloseConnection();
+    }
+}
