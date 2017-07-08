ed3bbf97313220ebf3373a0065a59aa120f2bd21
Andrea Vincenzo Abbondanza <andreabbondanza@outlook.com>
2017-07-08 09:27:43 +0200

    IValidate added

---
+++ DewInterfaces/DewExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DewInterfaces.DewExtensions
+{
+    public interface IValidate<T>
+    {
+        /// <summary>
+        /// Validate object with overwrite predicate
+        /// </summary>
+        /// <param name="overwrite"></param>
+        /// <returns></returns>
+        bool Validate(Func<T, bool> overwrite);
+        /// <summary>
+        /// Validate object
+        /// </summary>
+        /// <returns></returns>
+        bool Validate();
+    }
+}
